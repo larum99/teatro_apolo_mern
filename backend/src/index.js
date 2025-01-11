d1b856901bf9573be64cfcf39d06db3ede1e8139
Laura <laura.barona@uao.edu.co>
2025-01-11 13:32:02 -0500

    Deploy fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 4200;
+// const PORT = process.env.PORT || 4200;
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// // Iniciar el servidor
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
