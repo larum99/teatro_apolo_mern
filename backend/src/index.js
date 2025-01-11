0373a480a2709cd637014d07df994ff673d1c63a
Laura <laura.barona@uao.edu.co>
2025-01-11 13:19:01 -0500

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,8 @@
 const app = require("./app");
 
-module.exports = app; // Vercel se encargará de manejar el servidor
+const PORT = process.env.PORT || 4200;
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
