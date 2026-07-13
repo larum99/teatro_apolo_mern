95b8eed7f80502d3322a0627575a2ec7e0a2bab1
Laura <laura.barona@uao.edu.co>
2026-07-13 18:34:59 -0500

    Revert "Test"
    This reverts commit 302a6e560992ec0fe47713100e151d2aa357930c.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    console.log("URI:", process.env.MONGO_URI_CINEMA.replace(/\/\/.*:.*@/, "//***:***@"));
-
     const connCinema = await mongoose.connect(process.env.MONGO_URI_CINEMA);
-
-    console.log("Conectado:", connCinema.connection.host);
+    console.log(`Conectado a la base de datos cinema en MongoDB Atlas: ${connCinema.connection.host}`);
+    return connCinema; // Solo una conexión
   } catch (error) {
-    console.error(error);
+    console.error("Error al conectar a la base de datos:", error.message);
     throw error;
   }
 };
