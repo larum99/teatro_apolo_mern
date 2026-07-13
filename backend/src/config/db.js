302a6e560992ec0fe47713100e151d2aa357930c
Laura <laura.barona@uao.edu.co>
2026-07-13 12:03:43 -0500

    Test

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log("URI:", process.env.MONGO_URI_CINEMA.replace(/\/\/.*:.*@/, "//***:***@"));
+
     const connCinema = await mongoose.connect(process.env.MONGO_URI_CINEMA);
-    console.log(`Conectado a la base de datos cinema en MongoDB Atlas: ${connCinema.connection.host}`);
-    return connCinema; // Solo una conexión
+
+    console.log("Conectado:", connCinema.connection.host);
   } catch (error) {
-    console.error("Error al conectar a la base de datos:", error.message);
+    console.error(error);
     throw error;
   }
 };
